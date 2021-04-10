3c9b4b1d6fbccf067cd94abba2c110134d2c9b6f
Ryo-exid <sakura4oneok@gmail.com>
2021-04-10 08:19:05 +0000

    [Fix]最終課題

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_135857) do
+ActiveRecord::Schema.define(version: 2021_04_10_061704) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "profile_image_id"
+    t.text "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
