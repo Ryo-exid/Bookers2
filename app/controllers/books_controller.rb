ca6e4789186a7503b7cebb68fa1c534606d0a375
Ryo-exid <sakura4oneok@gmail.com>
2021-04-11 14:31:29 +0000

    bookのindexページの左側の作成(User info)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.find(params[:id])
   end
 
   def create
+    book = Book.new(_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
+    @users = User.all
+    @user = current_user
+    @user = User.find(current_user.id)
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+
   end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :prpfile_image)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    user = User.new(user_params)
+    user.save
+    redirect_to user_path(user.id)
   end
 
   def index
+    @users = User.all
+    @user = User.new(user_params)
+  end
+
+  def edit
+    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def show
@@ -13,6 +24,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def list_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
