cd23cb44350b38e9bf11ec37517366c4bebbfd14
Ryo-exid <sakura4oneok@gmail.com>
2021-04-11 07:57:49 +0000

    [Finish]loginページの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
 
@@ -16,5 +15,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path# ログアウト後に遷移するpathを設定
   end
+
+
 
 end
