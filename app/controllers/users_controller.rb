63a72fe1d1e2c1780a20b657b3b9625f472e249c
Ryo-exid <sakura4oneok@gmail.com>
2021-04-15 07:28:56 +0000

    [Finish]フラッシュメッセージの実装/booksの新規投稿が成功した時

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    user.save
-    redirect_to user_path(id: current_user)
-  end
 
   def index
     @users = User.all
