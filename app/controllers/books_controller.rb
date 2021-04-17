f5d1ab0d64f3c2fe4bbdf310b5b8ab35b56cf6d5
Ryo-exid <sakura4oneok@gmail.com>
2021-04-17 08:52:20 +0000

    [Fix]他人の投稿編集画面に遷移できない/リダイレクト先

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     unless @book.user == current_user
-      redirect_to book_path(current_user)
+      redirect_to books_path
     end
 
   end
