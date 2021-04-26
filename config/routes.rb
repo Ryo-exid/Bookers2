1d949575e90b3c1d9a06c4c9698a5beb4ca6135e
Ryo-exid <sakura4oneok@gmail.com>
2021-04-26 09:38:57 +0000

    [Fix]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update
-  ]
+  resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
 end
