c620f27d30d1216ea52a8e7647307bf76c990a91
Ryo-exid <sakura4oneok@gmail.com>
2021-05-10 05:27:49 +0000

    [Start]いいね機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_070153) do
+ActiveRecord::Schema.define(version: 2021_05_10_052645) do
 
   create_table "books", force: :cascade do |t|
     t.string "profile_image_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_04_12_070153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "favolites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
