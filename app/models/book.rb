cb3f233ba1e930759b02e52fd9baf14a731ee4f4
Ryo-exid <sakura4oneok@gmail.com>
2021-05-10 06:30:12 +0000

    [Add]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :title, presence: true #空でないように
   validates :body, presence: true, length: { maximum: 200 } # 「200文字以下」
 
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { in: 2..20 }  #「一意性をもたせる」 #「1文字以上20文字以下」
