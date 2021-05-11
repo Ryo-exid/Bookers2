2cca9723aac73e2bd0540f76be2de509ac785b48
Ryo-exid <sakura4oneok@gmail.com>
2021-05-11 13:04:25 +0000

    [Finish]「検索機能」を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => 'homes#about'
+  get 'search' => 'search#search'
   resources :books, only: [:create, :index, :show, :edit, :destroy, :update] do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
