f51b7dcff33e6c1b1c9000e84d54584513a38921
Ryo-exid <sakura4oneok@gmail.com>
2021-05-11 12:27:27 +0000

    [Finish]フォロー/フォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,28 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { in: 2..20 }  #「一意性をもたせる」 #「1文字以上20文字以下」
   validates :introduction, length: { maximum: 50 } # 「50文字以下」
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 
 end
